0b9bc09413c568956866f91ca2b667b7c5fb48a7
cryptoginger <keithlutes@gmail.com>
2022-06-13 06:16:31 -0600

    putting all my thoughts in comments/notes

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,30 @@
-import React from 'react';
+import { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import ContactForm from './Components/ContactForm';
 import DisplayContacts from './Components/DisplayContacts';
 
 function App() {
+  const [entries, setEntries] = useState([]);
   return (
-    //import Form
-    <ContactForm />
-    //<DisplayContacts />
+    <div className="App">
+      <ContactForm />
+      <br />
+      <DisplayContacts />
+    </div>
   );
 }
 
+function EntryForm() {
+  return(
+    //Formik form stuff here?
+  )
+}
+
 export default App;
+
+/*
+Still need to get the form to render and work,
+get the form submissions to display on screen with default sorting, alphabetical by name, with option to switch to email instead.
+Look more into Formik with email validation
+*/
